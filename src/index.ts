564c69bfca71be0c651314ad6cd095c5821a87fd
yash <shyash222@gmail.com>
2020-09-23 23:41:38 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+dotenv.config({ path: './config/config.env' });
+const app: Application = express();
+const PORT: number = 8800;
+const IP: string = process.env.IP || '0.0.0.0';
+app.get('/', (req, res) => {
+	res.send('hurray');
+});
+app.listen(PORT, IP, () => {
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
